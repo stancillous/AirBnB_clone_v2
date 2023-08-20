0a3bdd919394128a92398286a883aa7927611a75
raymond <stancillousray@gmail.com>
2023-08-20 13:32:22 +0300

    new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,3 +56,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+
+--script that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbhb_test_pwd';
+GRANT ALL PRIVILEGES ON hbhb_test_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT on perfomance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
