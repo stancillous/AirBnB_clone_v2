90f9c91452db97c0a378982d4e02ae70a5cfd493
raymond <stancillousray@gmail.com>
2023-08-20 13:11:56 +0300

    creating db and users

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev;
 
-CREATE USER 'hbnb_dev'@'localhost'
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT ALL PRIVILEGES ON 'hbnb_dev' TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
-CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbhb_test_pwd';
-GRANT ALL PRIVILEGES ON 'hbhb_test_db' TO 'hbnb_dev'@'localhost';
-GRANT SELECT on 'perfomance_schema' TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbhb_test_pwd';
+GRANT ALL PRIVILEGES ON hbhb_test_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT on perfomance_schema.* TO 'hbnb_dev'@'localhost';
