d5d74501a431607533fa0b81e375bc6067cec99c
raymond <stancillousray@gmail.com>
2023-08-20 13:40:36 +0300

    create db and user

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -58,10 +58,3 @@ class FileStorage:
             pass
 
 
---script that prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
-GRANT SELECT on perfomance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
