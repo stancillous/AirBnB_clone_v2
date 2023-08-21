bd302532f150be7a6639d117d96a92866f111ef6
raymond <stancillousray@gmail.com>
2023-08-21 15:38:52 +0300

    updated console.py

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,15 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        
+        dictionary= self.__dict__
+
+        if ('_sa_instance_state' in dictionary):
+            dictionary.pop('_sa_instance_state')
+
+
+        # return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls, self.id, dictionary)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
