5fe1775d129e4f369d012b410d392b0d84acdc19
raymond <stancillousray@gmail.com>
2023-09-06 18:00:15 +0300

    setup web statis

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install nginx -y
+
+if [ ! -d /data/ ]; then
+    mkdir -p /data/web_static/releases/test
+fi
+
+
+if [ ! -d /data/web_static/shared ]; then
+    mkdir -p /data/web_static/shared
+fi
+
+# if [ ! -d /data/web_static/current ]; then
+#     mkdir -p /data/web_static/current
+# fi
+
+cd /data/web_static/releases/test
+
+# create fake html file
+touch index.html
+
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" > index.html
+
+# check if sym link exists, delete it on every execution
+if [ -L /data/web_static/current ]; then
+    rm /data/web_static/current
+fi
+
+# create the symlink
+ln -s /data/web_static/releases/test /data/web_static/current 
+
+sudo chown -R ubuntu:ubuntu /data/
+
+http {
+
+    server {
+        listen 80;
+        server_name stanceray.tech;
+        root /var/www/html;
+
+        location /hbnb_static {
+            alias /data/web_static/current;
+        }
+    }
+
+}
+sudo nginx -s reload
