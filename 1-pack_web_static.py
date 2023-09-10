6d9e9318e84481bcd41ba100f056c91eb2c980aa
raymond <stancillousray@gmail.com>
2023-09-10 17:03:08 +0300

    compress files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 from fabric.api import local
-from datetime import datetime
+from time import strftime
+from datetime import date
 
 
 def do_pack():
-    """script that generates a .tgz archive from the
-    contents of the web_static folder"""
-    filedate = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"web_static_{filedate}.tgz"
+    """ A script that generates archive the contents of web_static folder"""
 
+    filename = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local(f"python -m tarfile -vc versions/{filename} web_static/")
-        return "versions/{filename}".format(filename)
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
     except Exception as e:
         return None
