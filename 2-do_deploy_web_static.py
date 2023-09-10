2eb9932dc44a0536e1a9e31f8b7774bbd8d4b754
raymond <stancillousray@gmail.com>
2023-09-11 00:03:11 +0300

    unpack tgz file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,34 @@
-#!/usr/bin/python3
+from fabric.api import env, put, run
+from fabric import *
+import os
 """that distributes an archive to your web servers,"""
 
+env.hosts = ['100.25.111.120', '54.152.132.135']
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/school"
+
+
 def do_deploy(archive_path):
-    """that distributes an archive to your web servers,"""
-    import os
+    """script that distributes an archive to your web servers"""
     if not os.path.exists(archive_path):
+        return False
+
+    archive_path_extension = archive_path[-18:-4]
+    try:
+        put(archive_path, '/tmp')
+        # exctract the tgz contents
+        run('sudo python3 -m tarfile -e /tmp/web_static_{}.tgz\
+             /data/web_static/releases/web_static_{}'
+            .format(archive_path_extension, archive_path_extension))
+
+        # remove the archive
+        run('sudo rm -r /tmp/web_static_{}.tgz'.format(archive_path_extension))
+
+        # delete this symbolic link
+        run('sudo rm -r /data/web_static/current')
+        # create new sym link
+        run('sudo ln -s /data/web_static/releases/web_static_{}\
+             /data/web_static/current'.format(archive_path_extension))
+        return True
+    except Exception as e:
         return False
