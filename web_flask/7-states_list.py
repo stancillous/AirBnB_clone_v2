24d2fc368691863de49ba0d538edde47528cdbf3
raymond <stancillousray@gmail.com>
2023-09-22 23:49:47 +0300

    states list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+"""script to start a flask web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+states = storage.all(State)  # returns a di ct
+states_values = states.values()  # should be  a list
+
+app = Flask("__name__")
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """func to serve the above route"""
+    return render_template('7-states_list.html', states_val=states_values)
+
+
+@app.teardown_appcontext
+def remove_session():
+    """func to remove the current Session after every request"""
+    storage.close()
