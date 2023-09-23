efe5db520106862512c83ff5eb7e77cf2d9bc2c1
raymond <stancillousray@gmail.com>
2023-09-23 11:53:51 +0300

    made files exec

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,3 +19,7 @@ def states_list():
 def remove_session(exception):
     """func to remove the current Session after every request"""
     storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
