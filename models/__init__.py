8816c599088378099668031a610d9cc2fdfee6b6
raymond <stancillousray@gmail.com>
2023-09-23 16:23:06 +0300

    fixed pycodestyle errors

--- models/__init__.py
+++ models/__init__.py
@@ -7,18 +7,10 @@ if (os.environ.get("HBNB_TYPE_STORAGE") == "db"):
     from models.engine.db_storage import DBStorage
     # print(f"\n\tBefore starting dbstorage\n")
     storage = DBStorage()
-    print(f"\n\tAfter starting the dbstorage\n")
     storage.reload()
 
 else:
     from models.engine.file_storage import FileStorage
-    print("file storage")
     storage = FileStorage()
     storage.reload()
 
-# import os
-# from models.engine.db_storage import DBStorage
-# storage = DBStorage()
-# print(os.environ.get("HBNB_TYPE_STORAGE"))
-# print(f"\n\tAfter starting dbstorage\n")
-# storage.reload()
