b1a175de2e5be9a375cddeb9c1bbae40e08756c5
raymond <stancillousray@gmail.com>
2023-09-25 00:20:02 +0300

    C:/Program Files/Git/python_route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-
 """python script that starts a Flask web application"""
+from flask import Flask
 
-if __name__ == '__main__':
-    from flask import Flask
-    app = Flask(__name__)
+app = Flask("__name__")
 
-    @app.route("/")
-    def hello():
-        """func to serve the above route"""
-        return "Hello HBNB!"
 
-    @app.route("/hbnb", strict_slashes=False)
-    def hbnb():
-        """func to serve the above route"""
-        return "HBNB"
+@app.route("/")
+def hello():
+    """func to serve the above route"""
+    return "Hello HBNB!"
 
-    @app.route("/c/<text>", strict_slashes=False)
-    def c(text):
-        """func to serve the above route"""
-        new_text = text.replace('_', ' ')
-        return "C % s" % new_text
 
-    @app.route("/python/", strict_slashes=False)
-    @app.route("/python/<text>", strict_slashes=False)
-    def python(text="is cool"):
-        """func to serve the above routes"""
-        new_text = text.replace('_', ' ')
-        return f"python % s" % new_text
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """func to serve the above route"""
+    return "HBNB"
 
-    app.run(host='0.0.0.0', port=5000, debug=None)
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """func to serve the above route"""
+    new_text = text.replace('_', ' ')
+    return "C % s" % new_text
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """func to serve the above routes"""
+    new_text = text.replace('_', ' ')
+    return "python % s" % new_text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
